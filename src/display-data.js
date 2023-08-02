bbea46fad23fce7aae8141118ccf94ea8094e22a
TonyLig <118136260+TonyLig@users.noreply.github.com>
2023-08-02 23:00:23 +0200

    code refactoring

--- src/display-data.js
+++ src/display-data.js
@@ -1,17 +1,12 @@
 // Display data
 export function displayData(response) {
   const data = response.docs;
+  const bookSection = document.getElementById("books-section");
+  const modal = document.getElementById("modal");
+  const modalParagraph = document.querySelector(".text-modal");
 
-  // // Looping data
-  data.forEach((booksArr) => {
-    createCard(booksArr);
-  });
-
-  // Create card
-  function createCard(book) {
-    // Target data
-    const modal = document.getElementById("modal");
-    const modalParagraph = document.querySelector(".text-modal");
+  // Loop through each book in the response data
+  data.forEach((book) => {
     const findTitle = book.title;
     const bookKey = book.key;
     const findAuthor = book.author_name
@@ -22,70 +17,47 @@ export function displayData(response) {
       ? `${coverUrl}`
       : `https://picsum.photos/id/777/500/500`;
 
-    // Create card element
+    // Create the elements for the book card
     const makeSlide = document.createElement("div");
-    const makeCard = document.createElement("div");
-    const makeImg = document.createElement("img");
-    const makeContent = document.createElement("div");
-    const makeTitle = document.createElement("div");
-    const makeAuthor = document.createElement("p");
-    const makeBtn = document.createElement("button");
-
-    // Append card element
-    const bookSection = document.getElementById("books-section");
-
-    // Append slide container
     makeSlide.className = "slide";
-    bookSection.appendChild(makeSlide);
 
-    // Append card container div
+    const makeCard = document.createElement("div");
     makeCard.className = "card";
-    makeSlide.appendChild(makeCard);
 
-    // Append Img
+    const makeImg = document.createElement("img");
     makeImg.className = "bk-cover";
     makeImg.src = coverImage;
     makeImg.alt = findTitle;
-    makeCard.appendChild(makeImg);
 
-    // Append content container div
+    const makeContent = document.createElement("div");
     makeContent.className = "content";
-    makeCard.appendChild(makeContent);
 
-    // Append title div
+    const makeTitle = document.createElement("div");
     makeTitle.className = "title";
-    makeTitle.appendChild(document.createTextNode(findTitle));
-    makeContent.appendChild(makeTitle);
+    makeTitle.textContent = findTitle;
 
-    // Append author P
+    const makeAuthor = document.createElement("p");
     makeAuthor.className = "author";
-    makeAuthor.appendChild(document.createTextNode(findAuthor));
-    makeContent.appendChild(makeAuthor);
+    makeAuthor.textContent = findAuthor;
 
-    //Button open modal
+    const makeBtn = document.createElement("button");
     makeBtn.className = "modal-button";
     makeBtn.setAttribute("id", "open-button");
-    makeBtn.appendChild(document.createTextNode("Learn More"));
-    makeContent.appendChild(makeBtn);
+    makeBtn.textContent = "Learn More";
 
-    //Retrive book description
+    // Add event listener to the "Learn More" button to fetch and show book description
     makeBtn.addEventListener("click", () => {
-      //Fetch API
       const descriptionUrl = `https://openlibrary.org${bookKey}.json`;
       fetch(descriptionUrl)
         .then((response) => response.json())
         .then((data) => {
-          //Handling description
           let description = "No description available";
-
           if (data.description) {
-            if (typeof data.description === "object") {
-              description = data.description.value || description;
-            } else if (typeof data.description === "string") {
-              description = data.description || description;
-            }
+            description =
+              typeof data.description === "object"
+                ? data.description.value || description
+                : data.description || description;
           }
-          // Show description
           modalParagraph.innerHTML = `${description}`;
           modal.showModal();
         })
@@ -94,5 +66,16 @@ export function displayData(response) {
           alert(`Sorry something goes wrong, try again: ${error.message}`);
         });
     });
-  }
+
+    // Append elements to create the book card
+    makeContent.appendChild(makeTitle);
+    makeContent.appendChild(makeAuthor);
+    makeContent.appendChild(makeBtn);
+
+    makeCard.appendChild(makeImg);
+    makeCard.appendChild(makeContent);
+
+    makeSlide.appendChild(makeCard);
+    bookSection.appendChild(makeSlide);
+  });
 }
--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ const btnSearch = document.querySelector(".btn-search");
 const cleanSection = document.getElementById("books-section");
 const modal = document.getElementById("modal");
 const closeModal = document.querySelector(".close-modal");
+const loader = document.getElementById("loader");
 
 // Add Event
 btnSearch.addEventListener("click", getInput);
-
 closeModal.addEventListener("click", () => {
   modal.close();
 });
@@ -19,30 +19,45 @@ closeModal.addEventListener("click", () => {
 // Function to get the input and manage the flow of the APP
 function getInput(event) {
   event.preventDefault();
-  getData(searchInput.value)
-    .then((cleanSection.innerHTML = ""))
-    .then(displayData)
-    .then(showLoader)
-    .catch((error) => {
-      console.error(error.message);
-      alert(`Sorry something goes wrong, try again: ${error.message}`);
-    });
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm === "") {
+    alert("Please enter a search term.");
+    return;
+  }
+
+  cleanBookSection();
+  getData(searchTerm).then((data) => {
+    if (data.docs.length === 0) {
+      alert("No data found for the given search term.");
+    } else {
+      displayData(data);
+      showLoader();
+    }
+  });
 }
 
 // Fetch API
-function getData(search) {
+async function getData(search) {
   const apiUrl = `https://openlibrary.org/search.json?limit=10&q=${search}`;
-  return fetch(apiUrl).then((response) => response.json());
+  const response = await fetch(apiUrl);
+  if (!response.ok) {
+    throw new Error("Failed to fetch data from the server.");
+  }
+  return response.json();
 }
 
-// Show the loader and afterwards put the focus on the books
+// Show the loader and afterwards put the focus on the books section
 function showLoader() {
-  const loader = document.getElementById("loader");
   loader.style.display = "block";
 
   setTimeout(function () {
     loader.style.display = "none";
     fullPageApi.moveTo(2);
-    document.querySelector(".search-bar").value = "";
-  }, 5000);
+    searchInput.value = "";
+  }, 3000);
+}
+
+// Clean up book section for next
+function cleanBookSection() {
+  cleanSection.innerHTML = "";
 }
